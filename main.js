edba03424cb1b3163dee10f91fec6452e8134681
itsreisv <123587174+itsreisv@users.noreply.github.com>
2023-04-12 12:01:09 -0400

    lord help me

---
+++ main.js
@@ -0,0 +1,62 @@
+
+let myLibrary = [];
+
+
+fullSend();
+function fullSend() {
+document.querySelector('#submit').addEventListener('click', () => {
+addBookToLibrary();
+addBooks();
+})}
+
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  }
+
+function addBookToLibrary(titleValue, authorValue, pagesValue) {
+     titleValue = document.querySelector('#title').value ;
+     authorValue = document.querySelector('#author').value ;
+     pagesValue = document.querySelector('#pages').value ;
+    new Book(titleValue, authorValue, pagesValue) ;
+    myLibrary.push(Book);
+   }
+
+function addBooks() {
+  for(Book of myLibrary) {
+    const container = document.querySelector("#card-box");
+    const content = document.createElement('div');
+    const title = document.createElement('p');
+    const author = document.createElement('p');
+    const pages = document.createElement('p');
+    const toggle = document.createElement('button');
+    const remove = document.createElement('button');
+    content.classList.add('card');
+    title.textContent =  document.querySelector('#title').value ;
+    content.appendChild(title);
+    author.textContent = document.querySelector('#author').value ;
+    content.appendChild(author);
+    pages.textContent =  document.querySelector('#pages').value ;
+    content.appendChild(pages);
+    toggle.textContent = 'Toggle';
+    content.appendChild(toggle);
+    remove.textContent = 'Remove';
+    content.appendChild(remove);
+    container.appendChild(content);
+ 
+
+  }
+}
+
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
